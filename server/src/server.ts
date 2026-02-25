7828c0fbe67662ebd4c1cb9b5ad4d1dd06c25f36
Sinval Luz <contato.sinvalluz@gmail.com>
2026-02-25 08:47:23 -0300

    chore: add biome.js and its configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,16 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
 
 const fastify = Fastify({
-  logger: true,
+	logger: true,
 });
 
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
+fastify.get('/', (_request, reply) => {
+	reply.send({ hello: 'world' });
 });
 
-fastify.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+fastify.listen({ port: 3000 }, (err, _address) => {
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
 });
